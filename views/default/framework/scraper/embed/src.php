372cdca45c264807f60e15c70d3be0bd980b2a47
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2014-11-21 16:55:04 +0300

    Better responsive embed view

--- views/default/framework/scraper/embed/src.php
+++ views/default/framework/scraper/embed/src.php
@@ -17,67 +17,65 @@ if (!$meta instanceof MetaHandler) {
 	return;
 }
 
+$module = elgg_extract('module', $vars, false);
 $classes = array(elgg_extract('class', $vars));
 
+$classes[] = 'embedder-block';
+$classes[] = 'clearfix';
+
 if ($meta->provider_name) {
-	$classes[] = 'embed-' . preg_replace('/[^a-z0-9\-]/i', '-', strtolower($meta->provider_name));
+	$classes[] = 'embedder-' . preg_replace('/[^a-z0-9\-]/i', '-', strtolower($meta->provider_name));
 }
 
 if ($meta->html) {
+	$classes[] = 'flex-video';
 	$body = str_replace('http://', '//', $meta->html);
 } else {
-	if ($meta->oembed_url && $meta->type == 'photo') {
-		$icon = elgg_view('output/url', array(
-			'text' => elgg_view('output/img', array(
-				'src' => str_replace('http://', '//', $meta->oembed_url),
-				'alt' => $meta->title,
-			)),
-			'href' => $meta->canonical,
-			'class' => 'embedder-photo',
-			'target' => '_blank',
+	if (!$module) {
+		$body .= '<h3>' . $meta->title . '</h3>';
+		$body .= elgg_view('output/url', array(
+			'text' => parse_url($meta->url, PHP_URL_HOST),
+			'href' => $meta->url,
+			'class' => 'embedder-link',
 		));
 	}
-	if ($meta->thumbnail_url) {
-		$icon = elgg_view('output/img', array(
-			'src' => $meta->thumbnail_url,
-			'width' => 100,
-			'class' => 'embedder-thumbnail',
-		));
-	}
-}
-
-if (!$body && $meta->description) {
 	$body .= elgg_view('output/longtext', array(
 		'value' => elgg_get_excerpt($meta->description),
 		'class' => 'embedder-description'
 	));
+	if ($meta->oembed_url && $meta->type == 'photo') {
+		$icon_url = $meta->canonical;
+	} else if ($meta->thumbnail_url) {
+		$icon_url = $meta->thumbnail_url;
+	}
+
+	if ($icon_url) {
+		$classes[] = 'embedder-has-icon';
+		$icon = elgg_view('output/url', array(
+			'class' => 'embedder-icon-bg',
+			'text' => '<span></span>',
+			'style' => 'background-image:url(' . $icon_url . ')',
+			'href' => $meta->url
+		));
+	}
 }
 
-if ($icon) {
-	$body = elgg_view_image_block($icon, $body, array(
-		'class' => 'embedder-image-block',
-	));
-}
+$body = elgg_view_image_block($icon, $body, array(
+	'class' => implode(' ', array_filter($classes))
+));
 
-if ($module = elgg_extract('module', $vars, 'embed')) {
+if ($module) {
 	$footer = elgg_view('output/url', array(
 		'href' => ($meta->canonical) ? $meta->canonical : $meta->url,
 		'target' => '_blank',
 	));
 
-	if (!$body) {
-		$body = $footer;
-		$footer = false;
-	}
+	$class = ($meta->type) ? "elgg-module-embedder-$meta->type" : '';
 
-	$classes[] = ($meta->type) ? "elgg-module-embed-$meta->type" : '';
-
-	$output = elgg_view_module($module, $meta->title, $body, array(
-		'class' => implode(' ', array_filter($classes)),
+	echo elgg_view_module($module, $meta->title, $body, array(
+		'class' => $class,
 		'footer' => $footer,
 	));
 } else {
-	$output = $body;
+	echo elgg_view_module('embed', false, $body);
 }
-
-echo $output;
