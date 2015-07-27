8932257810afb989d69a140fff449fecef8d278d
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2015-07-28 00:58:45 +0200

    chore(composer): dependency management

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ hypeScraper
 ![Elgg 1.9](https://img.shields.io/badge/Elgg-1.9.x-orange.svg?style=flat-square)
 ![Elgg 1.10](https://img.shields.io/badge/Elgg-1.10.x-orange.svg?style=flat-square)
 ![Elgg 1.11](https://img.shields.io/badge/Elgg-1.11.x-orange.svg?style=flat-square)
+![Elgg 1.12](https://img.shields.io/badge/Elgg-1.12.x-orange.svg?style=flat-square)
 
 A tool for extracting, interpreting, caching and embedding remote resources.
 
--- lib/autoloader.php
+++ lib/autoloader.php
@@ -4,13 +4,13 @@ if (!is_callable('hypeApps')) {
 	throw new Exception("hypeScraper requires hypeApps");
 }
 
-$path = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR;
+$path = dirname(dirname(dirname(dirname(__FILE__))));
 
-if (!file_exists("{$path}vendor/autoload.php")) {
+if (!file_exists("{$path}/vendor/autoload.php")) {
 	throw new Exception('hypeScraper can not resolve composer dependencies. Run composer install');
 }
 
-require_once "{$path}vendor/autoload.php";
+require_once "{$path}/vendor/autoload.php";
 
 /**
  * Plugin container
