52ac5dec161ec5ff9a72335853097bc1ccbf17a4
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-11-20 16:55:13 +0100

    Add FR translation

---
+++ languages/fr.php
@@ -0,0 +1,21 @@
+<?php
+
+$french = array(
+	'scraper:settings:service' => "Service d'extraction",
+	'scraper:settings:service:help' => "Indiquez le service d'extraction à utiliser. Si vous utilisez un autre service que le parseur dOm natif, '
+	. 'indiquez la configuraiton nécessaire ci-dessous",
+	'scraper:settings:service:dom_parser' => "Parseur DOM natif",
+	'scraper:settings:service:iframely' => "iframe.ly",
+	'scraper:settings:service:embedly' => "embed.ly",
+	'scraper:settings:iframely:endpoint' => "Endpoint oEmbed de iframe.ly",
+	'scraper:settings:iframely:endpoint:help' => "par ex. http://iframe.ly/api/oembed",
+	'scraper:settings:iframely:key' => "Clef d'API iframe.ly",
+	'scraper:settings:iframely:key:help' => "Une clef d'API valide pour iframe.ly",
+	'scraper:settings:embedly:endpoint' => "Endpoint oEmbed de embed.ly",
+	'scraper:settings:embedly:endpoint:help' => "par ex. http://api.embed.ly/1/oembed",
+	'scraper:settings:embedly' => "Clef d'API Embed.ly",
+	'scraper:settings:embedly:help' => "Une clef d'API valide pour embed.ly",
+);
+
+add_translation('fr', $french);
+
