b25c012b2c0f40bdec68dc6bc0b8e00d3df867ef
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2016-11-20 16:47:41 +0100

    Add FR translation

---
+++ languages/fr.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	'scraper:settings:linkify' => "Transformer les liens dans l'affichage d'une zone de texte",
+	'scraper:settings:linkify:help' => "Ajoute automatiquement les balises autour des URLs, des noms d'utilisateur, des hashtags et des emails dans la vue d'affichage des zone de texte",
+
+	'scraper:settings:bookmarks' => "Ajouter une prévisualisation aux liens web",
+	'scraper:settings:bookmarks:help' => 'Ajoute une prévisualoisation des liens web dans la rivière et la vue en pleine page',
+	
+];
