38f53f610e66a1e42c7dd82c11902af5feba948c
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-03-22 14:27:25 +0100

    chore(security): clean up regex matching

--- classes/hypeJunction/Scraper/Views.php
+++ classes/hypeJunction/Scraper/Views.php
@@ -161,7 +161,7 @@ class Views {
 	 * @return array
 	 */
 	public static function cleanEmbedHTML($hook, $type, $return, $params) {
-
+		
 		if (empty($return['html'])) {
 			return;
 		}
@@ -170,20 +170,20 @@ class Views {
 		$canonical_url = parse_url(elgg_extract('canonical', $return, ''), PHP_URL_HOST);
 
 		$domains = self::getoEmbedDomains();
-		$domains = array_map(function($elem) {
-			return preg_quote($elem);
+		
+		$matches = array_map(function($elem) use ($url, $canonical_url) {
+			$elem = preg_quote($elem);
+			$domain_pattern = "/(.+?)?$elem/i";
+			return preg_match($domain_pattern, $url) || preg_match($domain_pattern, $canonical_url);
 		}, $domains);
-		$domains = implode('|', $domains);
 
-		$domain_pattern = "/(.+?)($domains)/";
-
+		$matches = array_filter($matches);
+		
 		// only allow html from whitelisted domains
-		if (!preg_match($domain_pattern, $url) && !preg_match($domain_pattern, $canonical_url)) {
+		if (empty($matches)) {
 			unset($return['html']);
-		}
-
-		// only allow iframe, video, and audio tags
-		if (!preg_match('/<iframe|video|audio/i', $return['html'])) {
+		} else if (!preg_match('/<iframe|video|audio/i', $return['html'])) {
+			// only allow iframe, video, and audio tags
 			unset($return['html']);
 		}
 
